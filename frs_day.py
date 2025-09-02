6db8708fdbe5c19996c8f00003ac5086be53e74b
Xromk_ep55 <fortniteoldfort@gmail.com>
2025-09-02 09:43:53 +0300

    FIRST LESSON

---
+++ frs_day.py
@@ -0,0 +1,8 @@
+a = input("whats your first name?")
+b = input('whats your surname?')
+c = input('whats your hobby?')
+print("First name:", a)
+print("Last name:", b)
+print("Hobby:", c)
+
+# змінна - іменованна область оперативної пам'яті яка зберіє будь-яке значення
