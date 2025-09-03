eb2d1de9cfbeb87f13689513b17726f35fbc0d34
Xromk_ep55 <fortniteoldfort@gmail.com>
2025-09-03 11:52:19 +0300

    2 lesson

---
+++ scnd_lesson.py
@@ -0,0 +1,11 @@
+# name = input("Enter your name:")
+# surname = input("Enter your Surname:")
+# age = int(input("Enter your age:"))
+#
+# print("           \n        Your Application Form:")
+# print(f"              age: {age}")
+# print(f"              name: {name}")
+# print(f"              Surname: {surname}")
+
+brainrot = input("Write your brainrot that you like:")
+print(f"Brainrot: {brainrot}")
